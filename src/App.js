c25b838fb54637f408dbf99fc49e252ae071003d
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-10-17 21:07:52 +0200

    created header skeleton in html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import { MainWrapper } from "./styled";
+import { MainWrapper } from "./MainWrapper/styled";
 
 function App() {
-  return (
-    <div className="App">
-      <MainWrapper />
-    </div>
-  );
+	return (
+		<div className="App">
+			<MainWrapper>
+				<header className="finderSection">
+					<div className="userCities">
+						<ul className="citiesList">
+							<li className="citiesItem">
+								Londyn
+							</li>
+							<li className="citiesItem">
+								Paryz
+							</li>
+						</ul>
+					</div>
+					<div className="container">
+						<div className="locationOptions">
+							<input className="citySearch"></input>
+							<div className="buttons">
+								<button className="button"></button>
+								<button className="button"></button>
+							</div>
+						</div>
+						<div className="temperatureOptions">
+							<div className="buttons">
+								<button className="button"></button>
+								<button className="button"></button>
+							</div>
+						</div>
+					</div>
+					<div className="dateContainer">
+						<p className="date"></p>
+					</div>
+				</header>
+			</MainWrapper>
+		</div>
+	);
 }
 
 export default App;
--- src/MainWrapper/styled.js
+++ src/MainWrapper/styled.js
@@ -4,5 +4,4 @@ export const MainWrapper = styled.main`
     max-width: 900px;
     width: 100%;
     height: 100vh;
-    background-color: red;
 `;
