45209327bf228fb94b1e118c976a9bb5b5b6ea75
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-10-19 22:58:02 +0200

    styled part of PlaceWeatherInformation component

--- src/MainWrapper/PlaceInformation/styled.js
+++ src/MainWrapper/PlaceInformation/styled.js
@@ -1,4 +1,25 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const WeatherInformation = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    justify-content: space-around;
+`;
+
+export const WeatherImage = styled.div`
+    width: 100px;
+    height: 100px;
+    background-color: black;
+    border-radius: 50%;
+`;
+
+export const TemperatureElement = styled.p`
+    font-size: 40px;
 `;
