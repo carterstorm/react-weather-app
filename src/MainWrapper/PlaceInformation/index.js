ce55a6276e1d7fd57842079e320fd92d5291a592
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-10-19 22:53:53 +0200

    created PlaceWeatherInformation component skeleton

--- src/MainWrapper/PlaceInformation/index.js
+++ src/MainWrapper/PlaceInformation/index.js
@@ -1,7 +1,29 @@
-import { Wrapper } from "./styled";
+import { TemperatureElement, WeatherImage, WeatherInformation, Wrapper } from "./styled";
 
 export const PlaceWeatherInformation = () => (
     <Wrapper>
-
+        <p>Kraków</p>
+        <p>Pochmurno</p>
+        <WeatherInformation>
+            <WeatherImage></WeatherImage>
+            <TemperatureElement>34°</TemperatureElement>
+            <div>
+                <div>
+                    <span></span>
+                    <p></p>
+                    <span></span>
+                </div>
+                <div>
+                    <span></span>
+                    <p></p>
+                    <span></span>
+                </div>
+                <div>
+                    <span></span>
+                    <p></p>
+                    <span></span>
+                </div>
+            </div>
+        </WeatherInformation>
     </Wrapper>
 );
