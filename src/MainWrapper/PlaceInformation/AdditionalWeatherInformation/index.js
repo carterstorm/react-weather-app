776cd7656bdabe1fc249936d3f917c2acc1fa887
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-10-23 19:21:50 +0200

    added svg icons to code

--- src/MainWrapper/PlaceInformation/AdditionalWeatherInformation/index.js
+++ src/MainWrapper/PlaceInformation/AdditionalWeatherInformation/index.js
@@ -1,25 +1,29 @@
 import { WeatherItem } from "../WeatherItem";
 import { Wrapper } from "./styled";
+import sunrise from "../../../svg/sunrise.svg";
+import sunset from "../../../svg/sunset.svg";
+import max_temp from "../../../svg/max_temp.svg";
+import min_temp from "../../../svg/min_temp.svg";
 
 export const AdditionalWeatherInformation = () => (
     <Wrapper>
         <WeatherItem
-            url={"example"}
+            url={sunrise}
             text={"Wschód:"}
             data={"7:11"}
         />
         <WeatherItem
-            url={"example"}
+            url={sunset}
             text={"Zachód:"}
             data={"17:58"}
         />
         <WeatherItem
-            url={"example"}
+            url={max_temp}
             text={"Max. temperatura:"}
             data={"17"}
         />
         <WeatherItem
-            url={"example"}
+            url={min_temp}
             text={"Min. temperatura:"}
             data={"2"}
         />
