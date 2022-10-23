016a9f89b81cfd708e59deba06fa5ecdec0d9409
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-10-23 21:01:34 +0200

    styled Forecast elements

--- src/MainWrapper/Forecast/index.js
+++ src/MainWrapper/Forecast/index.js
@@ -1,13 +1,45 @@
-import { ForecastItem, Heading, Temperature, Time, WeatherImage, Wrapper } from "./styled";
+import { ForecastContainer, ForecastItem, Heading, Temperature, Time, WeatherImage, Wrapper } from "./styled";
 import rainy from "../../svg/rainy.svg"
 
 export const Forecast = () => (
     <Wrapper>
         <Heading>hourly forecast</Heading>
-        <ForecastItem>
-            <Time>10:00</Time>
-            <WeatherImage src={rainy} />
-            <Temperature>12°</Temperature>
-        </ForecastItem>
+        <ForecastContainer>
+            <ForecastItem>
+                <Time>10:00</Time>
+                <WeatherImage src={rainy} />
+                <Temperature>12°</Temperature>
+            </ForecastItem>
+
+            <ForecastItem>
+                <Time>10:00</Time>
+                <WeatherImage src={rainy} />
+                <Temperature>12°</Temperature>
+            </ForecastItem>
+
+            <ForecastItem>
+                <Time>10:00</Time>
+                <WeatherImage src={rainy} />
+                <Temperature>12°</Temperature>
+            </ForecastItem>
+
+            <ForecastItem>
+                <Time>10:00</Time>
+                <WeatherImage src={rainy} />
+                <Temperature>12°</Temperature>
+            </ForecastItem>
+
+            <ForecastItem>
+                <Time>10:00</Time>
+                <WeatherImage src={rainy} />
+                <Temperature>12°</Temperature>
+            </ForecastItem>
+
+            <ForecastItem>
+                <Time>10:00</Time>
+                <WeatherImage src={rainy} />
+                <Temperature>12°</Temperature>
+            </ForecastItem>
+        </ForecastContainer>
     </Wrapper>
 ); 
--- src/MainWrapper/Forecast/styled.js
+++ src/MainWrapper/Forecast/styled.js
@@ -12,14 +12,25 @@ export const Heading = styled.h1`
     border-bottom: 1px solid black;
 `;
 
+export const ForecastContainer = styled.section`
+    display: flex;
+    justify-content: space-around;
+`;
+
 export const ForecastItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `;
 
 export const Time = styled.p`
+    font-size: 12px;
 `;
 
 export const WeatherImage = styled.img`
+    width: 60px;
 `;
 
 export const Temperature = styled.p`
+    font-weight: 700;
 `;
