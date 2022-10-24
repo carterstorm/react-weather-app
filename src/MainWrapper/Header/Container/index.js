ff8fe5420acae644d7c8933a8a0057cf31b92e3e
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-10-24 22:03:27 +0200

    get data from geolocation API

--- src/MainWrapper/Header/Container/index.js
+++ src/MainWrapper/Header/Container/index.js
@@ -2,12 +2,16 @@ import { Button } from "../Button";
 import { CitySearchInput, Location, TemperatureButtons, Wrapper } from "./styled";
 import search from "../../../svg/search.svg";
 import location from "../../../svg/location.svg";
+import { useGeolocation } from "../../../useGeolocationApi";
 
 export const Container = () => {
 
     const onFormSubmit = (event) => {
         event.preventDefault();
     }
+
+    const locationData = useGeolocation();
+    console.log(locationData);
 
     return (
         <Wrapper>
---
+++ src/useGeolocationApi.js
@@ -0,0 +1,37 @@
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const GEO_API_BASE_URL = "http://api.openweathermap.org/geo/1.0/direct?";
+const API_KEY = "5fb98d393480921290c1663a251e6493";
+const city = "Warsaw"
+
+export const useGeolocation = () => {
+    const [locationData, setApiData] = useState({
+        state: "loading",
+    });
+
+    useEffect(() => {
+        const getLocation = async () => {
+            try {
+                const response = await axios.get(`${GEO_API_BASE_URL}q=${city}&limit=5&appid=${API_KEY}`);
+                const lat = response.data[0].lat;
+                const lon = response.data[0].lon;
+
+                setApiData({
+                    state: "succes",
+                    latitude: lat,
+                    longitude: lon,
+                });
+            } catch {
+                setApiData({
+                    state: "error",
+                });
+            }
+        };
+
+        setTimeout(getLocation, 2 * 1000);
+    }, []);
+
+    return locationData;
+};
