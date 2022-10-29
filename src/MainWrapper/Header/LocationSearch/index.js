0de6a845233a38ff6857d9267abe896f64a10e2e
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-10-29 21:51:54 +0200

    Added onChange handler

--- src/MainWrapper/Header/LocationSearch/index.js
+++ src/MainWrapper/Header/LocationSearch/index.js
@@ -1,4 +1,5 @@
 
+import { useState } from "react";
 import searchImage from "../../../svg/search.svg";
 import location from "../../../svg/location.svg";
 import { SearchInput, StyledLocationSearch } from "./styled";
@@ -6,13 +7,22 @@ import { Button } from "../Button";
 
 export const LocationSearch = () => {
 
+    const [search, setSearch] = useState("");
+
     const onFormSubmit = (event) => {
         event.preventDefault();
     };
 
+    const onInputChange = (event) => {
+        setSearch(event.target.value);
+    };
+
     return (
         <StyledLocationSearch onSubmit={onFormSubmit}>
-            <SearchInput />
+            <SearchInput
+                value={search}
+                onChange={onInputChange}
+            />
             <Button img={searchImage} />
             <Button img={location} />
         </StyledLocationSearch>
