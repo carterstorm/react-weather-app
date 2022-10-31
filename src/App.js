f8dbb069af971f3b5bc26a087efb00d60f476ffe
Sebastian Komorowski <sebastiano.komo@hotmail.com>
2022-10-31 17:57:54 +0100

    changed place of all components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,94 @@
+import { useState } from "react";
+import searchImage from "./svg/search.svg";
+import locationImage from "./svg/location.svg";
+
+import {
+	CitiesItem,
+	CitiesList,
+	Container,
+	HeaderWrapper,
+	MainWrapper,
+	SearchInput,
+	StyledLocationSearch,
+	TemperatureButtons
+} from "./MainWrapper/styled";
+import { Button } from "./MainWrapper/Button";
+import { DateContainer } from "./MainWrapper/DateContainer";
 import { Forecast } from "./MainWrapper/Forecast";
-import { Header } from "./MainWrapper/Header";
 import { PlaceWeatherInformation } from "./MainWrapper/PlaceInformation";
-import { MainWrapper } from "./MainWrapper/styled";
+
+import { getLocationApi } from "./getLocationApi";
+
 
 function App() {
+
+	const [search, setSearch] = useState("");
+
+	const onFormSubmit = (event) => {
+		event.preventDefault();
+		getLocationApi(search);
+	};
+
+	const onInputChange = (event) => {
+		setSearch(event.target.value);
+		console.log("trololo")
+	};
+
+
 	return (
 		<div className="App">
 			<MainWrapper>
-				<Header />
+				<HeaderWrapper>
+					<CitiesList>
+						<CitiesItem>
+							Londyn
+						</CitiesItem>
+						<CitiesItem>
+							Paryż
+						</CitiesItem>
+						<CitiesItem>
+							Warszawa
+						</CitiesItem>
+						<CitiesItem>
+							Belgrad
+						</CitiesItem>
+						<CitiesItem>
+							Praga
+						</CitiesItem>
+					</CitiesList>
+					<Container>
+						<StyledLocationSearch
+							onSubmit={onFormSubmit}>
+							<SearchInput
+								value={search}
+								onChange={onInputChange}
+							/>
+							<Button
+								img={searchImage}
+							/>
+							<Button
+								img={locationImage}
+							/>
+						</StyledLocationSearch>
+
+						<TemperatureButtons>
+							<Button
+								content={"°C"}
+							/>
+							{"|"}
+							<Button
+								content={"°F"}
+							/>
+						</TemperatureButtons>
+					</Container >
+
+					<DateContainer />
+				</HeaderWrapper>
+
+
 				<PlaceWeatherInformation />
 				<Forecast />
+
 			</MainWrapper>
 		</div>
 	);
