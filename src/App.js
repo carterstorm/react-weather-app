ff29fbcb2cab17b9324a738ba215da90d1b9e633
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2022-11-01 10:45:44 +0100

    deleted Header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
+import { useState } from "react";
+import searchImage from "./svg/search.svg";
+import locationImage from "./svg/location.svg";
+import { useApiData } from "./useApiData";
+import { DateContainer } from "./MainWrapper/DateContainer";
+
 import { Forecast } from "./MainWrapper/Forecast";
-import { Header } from "./MainWrapper/Header";
+import { Button } from "./MainWrapper/Button";
 import { PlaceWeatherInformation } from "./MainWrapper/PlaceInformation";
-import { MainWrapper } from "./MainWrapper/styled";
+import { CitiesItem, CitiesList, Container, Header, LocationSearch, MainWrapper, SearchInput, TemperatureButtons } from "./MainWrapper/styled";
+
 
 function App() {
+
+	const [search, setSearch] = useState("");
+
+	const apiData = useApiData(search);
+	console.log(apiData);
+
+	const onFormSubmit = (event) => {
+		event.preventDefault();
+	};
+
+	const onInputChange = (event) => {
+		setSearch(event.target.value);
+	};
+
 	return (
 		<div className="App">
 			<MainWrapper>
-				<Header />
+				<Header>
+					<CitiesList>
+						<CitiesItem>
+							Londyn
+						</CitiesItem>
+						<CitiesItem>
+							Paryż
+						</CitiesItem>
+						<CitiesItem>
+							Warszawa
+						</CitiesItem>
+						<CitiesItem>
+							Belgrad
+						</CitiesItem>
+						<CitiesItem>
+							Praga
+						</CitiesItem>
+					</CitiesList>
+					<Container>
+						<LocationSearch onSubmit={onFormSubmit}>
+							<SearchInput value={search} onChange={onInputChange} />
+							<Button img={searchImage} />
+							<Button img={locationImage} />
+						</LocationSearch>
+						<TemperatureButtons>
+							<Button content={"°C"} />
+							{"|"}
+							<Button content={"°F"} />
+						</TemperatureButtons>
+					</Container >
+					<DateContainer />
+				</Header>
 				<PlaceWeatherInformation />
 				<Forecast />
 			</MainWrapper>
--- src/MainWrapper/styled.js
+++ src/MainWrapper/styled.js
@@ -5,3 +5,42 @@ export const MainWrapper = styled.main`
     width: 100%;
     height: 100vh;
 `;
+
+export const Header = styled.header`
+    padding: 0 20px;
+`;
+
+export const CitiesList = styled.ul`
+    display: flex;
+    justify-content: space-around;
+    list-style: none;
+    padding: 0;
+`;
+
+export const CitiesItem = styled.li`
+    padding: 5px 10px;
+`;
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const TemperatureButtons = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+export const LocationSearch = styled.form`
+    display: flex;
+    align-items: center;
+`;
+
+export const SearchInput = styled.input`
+    min-width: 400px;
+    height: 30px;
+    margin-right: 10px;
+    padding: 0 10px;
+    border-radius: 5px;
+    border: 1px solid black;
+`;
