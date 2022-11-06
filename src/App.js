c8e2021d60e4119d8b57b8dd1f8e0a052979a148
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2022-11-06 18:17:05 +0100

    select from scratch...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import searchImage from "./svg/search.svg";
 import locationImage from "./svg/location.svg";
 import { DateContainer } from "./MainWrapper/DateContainer";
@@ -6,23 +5,19 @@ import { DateContainer } from "./MainWrapper/DateContainer";
 import { Forecast } from "./MainWrapper/Forecast";
 import { Button } from "./MainWrapper/Button";
 import { PlaceWeatherInformation } from "./MainWrapper/PlaceInformation";
-import { CitiesItem, CitiesList, Container, Header, LocationSearch, MainWrapper, SearchInput, TemperatureButtons } from "./MainWrapper/styled";
-import { useApiLocationData } from "./useApiLocation";
+import {
+	CitiesItem,
+	CitiesList,
+	Container,
+	Header,
+	LocationSearch,
+	MainWrapper,
+	SearchInput,
+	TemperatureButtons
+} from "./MainWrapper/styled";
 
 
 function App() {
-
-	const [search, setSearch] = useState("");
-
-	const onFormSubmit = (event) => {
-		event.preventDefault();
-	};
-
-	const onInputChange = (event) => {
-		setSearch(event.target.value);
-	};
-
-	const apiLocationData = useApiLocationData();
 
 	return (
 		<div className="App">
@@ -46,8 +41,8 @@ function App() {
 						</CitiesItem>
 					</CitiesList>
 					<Container>
-						<LocationSearch onSubmit={onFormSubmit}>
-							<SearchInput value={search} onChange={onInputChange} />
+						<LocationSearch>
+							<SearchInput />
 							<Button img={searchImage} />
 							<Button img={locationImage} />
 						</LocationSearch>
