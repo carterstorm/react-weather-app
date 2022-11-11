6905ba4da7e99eddf9b3df3d8c48499c8fb18678
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2022-11-11 14:51:56 +0100

    added capitalizeFirstLetter function

--- src/MainWrapper/PlaceInformation/index.js
+++ src/MainWrapper/PlaceInformation/index.js
@@ -4,10 +4,12 @@ import { WeatherBasicInformation } from "./WeatherBasicInformation";
 
 export const PlaceWeatherInformation = ({ apiSearch }) => {
 
+    const capitalizeFirstLetter = (item) => item.charAt(0).toUpperCase() + item.slice(1);
+
     return (
         <Wrapper>
             <Place>{apiSearch.name} - {apiSearch.country}</Place>
-            <SkyInfo>Pochmurno</SkyInfo>
+            <SkyInfo>{capitalizeFirstLetter(apiSearch.description)}</SkyInfo>
             <WeatherInformation>
                 <WeatherImage />
                 <WeatherTemperature>34°</WeatherTemperature>
