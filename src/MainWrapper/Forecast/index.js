26fd8d9d90cf7895b28608af1c4c7fda62091d2a
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2022-11-23 23:07:10 +0100

    added changeTIme func which convert time from api

--- src/MainWrapper/Forecast/index.js
+++ src/MainWrapper/Forecast/index.js
@@ -1,12 +1,21 @@
 import { ForecastContainer, Heading, Wrapper } from "./styled";
 import rainy from "../../svg/rainy.svg"
 import { ForecastItem } from "./ForecastItem";
+import { getHoursAndMinutes } from "../../getHoursAndMinutes";
 
-export const Forecast = () => (
-    <Wrapper>
-        <Heading>temperatura godzinowa</Heading>
-        <ForecastContainer>
-            <ForecastItem img={rainy}></ForecastItem>
-        </ForecastContainer>
-    </Wrapper>
-); 
+export const Forecast = ({ apiSearch }) => {
+
+    const changeTime = apiSearch.data.map(({ dt }) => {
+        const time = new Date(dt * 1000);
+        return getHoursAndMinutes(time)
+    });
+
+    return (
+        <Wrapper>
+            <Heading>temperatura godzinowa</Heading>
+            <ForecastContainer>
+                <ForecastItem img={rainy}></ForecastItem>
+            </ForecastContainer>
+        </Wrapper>
+    )
+};
